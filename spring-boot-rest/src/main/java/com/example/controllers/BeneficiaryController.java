18e7ffc7c9b7c6526ad12ba1c5eb728ebd70bb38
luizgcserra <luizgcserra@gmail.com>
2018-06-26 22:49:42 -0300

    Added Contract<>Model Parser

--- spring-boot-rest/src/main/java/com/example/controllers/BeneficiaryController.java
+++ spring-boot-rest/src/main/java/com/example/controllers/BeneficiaryController.java
@@ -2,7 +2,6 @@ package com.example.controllers;
 
 import java.net.URI;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -21,6 +20,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.example.model.BeneficiaryKey;
+import com.example.parsers.BeneficiaryParser;
 import com.example.repository.BeneficiaryRepository;
 import com.google.common.collect.Lists;
 
@@ -42,15 +42,9 @@ public class BeneficiaryController {
 		if (modelList != null) {
 			List<com.example.contracts.Beneficiary> contractList = new ArrayList<com.example.contracts.Beneficiary>(
 					modelList.size());
-			ModelMapper mapper = new ModelMapper();
-			com.example.contracts.Beneficiary contract = null;
 
 			for (com.example.model.Beneficiary model : modelList) {
-				contract = mapper.map(model, com.example.contracts.Beneficiary.class);
-				contract.setNisBeneficiario(model.getKey().getNisBeneficiario());
-				contract.setMesCompetencia(model.getKey().getMesCompetencia());
-
-				contractList.add(contract);
+				contractList.add(BeneficiaryParser.Parse(model));
 			}
 
 			return ResponseEntity.ok(contractList);
@@ -62,16 +56,11 @@ public class BeneficiaryController {
 	@GetMapping(value = "/{id}/{competency}", headers = "Accept=application/json")
 	public ResponseEntity<?> getOne(@PathVariable(value = "id") String id,
 			@PathVariable(value = "competency") String competency) {
-		com.example.model.Beneficiary beneficiary = this.beneficiaryRepository.findOne(new BeneficiaryKey(id, competency));
-		ModelMapper mapper = new ModelMapper();
+		com.example.model.Beneficiary beneficiary = this.beneficiaryRepository
+				.findOne(new BeneficiaryKey(id, competency));
 
 		if (beneficiary != null) {
-			com.example.contracts.Beneficiary contract = mapper.map(beneficiary,
-					com.example.contracts.Beneficiary.class);
-			contract.setNisBeneficiario(beneficiary.getKey().getNisBeneficiario());
-			contract.setMesCompetencia(beneficiary.getKey().getMesCompetencia());
-
-			return ResponseEntity.ok(contract);
+			return ResponseEntity.ok(BeneficiaryParser.Parse(beneficiary));
 		} else {
 			return ResponseEntity.noContent().build();
 		}
@@ -79,11 +68,7 @@ public class BeneficiaryController {
 
 	@PostMapping(headers = "Accept=application/json")
 	public ResponseEntity<?> add(@RequestBody com.example.contracts.Beneficiary input) {
-		ModelMapper mapper = new ModelMapper();
-		com.example.model.Beneficiary model = mapper.map(input, com.example.model.Beneficiary.class);
-		model.setKey(new BeneficiaryKey(input.getNisBeneficiario(), input.getMesCompetencia().replace("/", "")));
-
-		com.example.model.Beneficiary result = this.beneficiaryRepository.save(model);
+		com.example.model.Beneficiary result = this.beneficiaryRepository.save(BeneficiaryParser.Parse(input));
 
 		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
 				.buildAndExpand(result.getKey().getNisBeneficiario()).toUri();
@@ -94,11 +79,7 @@ public class BeneficiaryController {
 	@PutMapping(headers = "Accept=application/json")
 	public ResponseEntity<?> modify(@RequestBody com.example.contracts.Beneficiary input) {
 		if (input != null && input.getNisBeneficiario().length() > 0) {
-			ModelMapper mapper = new ModelMapper();
-			com.example.model.Beneficiary model = mapper.map(input, com.example.model.Beneficiary.class);
-			model.setKey(new BeneficiaryKey(input.getNisBeneficiario(), input.getMesCompetencia().replace("/", "")));
-
-			this.beneficiaryRepository.save(mapper.map(model, com.example.model.Beneficiary.class));
+			this.beneficiaryRepository.save(BeneficiaryParser.Parse(input));
 
 			return ResponseEntity.ok(input);
 		} else {
@@ -109,9 +90,9 @@ public class BeneficiaryController {
 	@DeleteMapping(value = "/{id}", headers = "Accept=application/json")
 	public ResponseEntity<?> delete(@PathVariable(value = "id") String id) {
 		if (id != null && id.length() > 0) {
-			this.beneficiaryRepository.findByNis(id).forEach(b-> {
+			this.beneficiaryRepository.findByNis(id).forEach(b -> {
 				this.beneficiaryRepository.delete(new BeneficiaryKey(id, b.getKey().getMesCompetencia()));
-			});		
+			});
 
 			return ResponseEntity.ok().build();
 		} else {
@@ -120,7 +101,8 @@ public class BeneficiaryController {
 	}
 
 	@RequestMapping(value = "/", method = RequestMethod.GET, headers = "Accept=application/json")
-	public ResponseEntity<?> getAll(@RequestParam(value = "nomeBeneficiario", required = false) String nomeBeneficiario) {
+	public ResponseEntity<?> getAll(
+			@RequestParam(value = "nomeBeneficiario", required = false) String nomeBeneficiario) {
 		List<com.example.model.Beneficiary> modelList = null;
 
 		if (nomeBeneficiario != null) {
@@ -132,15 +114,9 @@ public class BeneficiaryController {
 		if (modelList != null) {
 			List<com.example.contracts.Beneficiary> contractList = new ArrayList<com.example.contracts.Beneficiary>(
 					modelList.size());
-			ModelMapper mapper = new ModelMapper();
-			com.example.contracts.Beneficiary contract = null;
 
 			for (com.example.model.Beneficiary model : modelList) {
-				contract = mapper.map(model, com.example.contracts.Beneficiary.class);
-				contract.setNisBeneficiario(model.getKey().getNisBeneficiario());
-				contract.setMesCompetencia(model.getKey().getMesCompetencia());
-
-				contractList.add(contract);
+				contractList.add(BeneficiaryParser.Parse(model));
 			}
 
 			return ResponseEntity.ok(contractList);
